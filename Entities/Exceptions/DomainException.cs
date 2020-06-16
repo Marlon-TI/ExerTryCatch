9d7a36c8a995da908361c5c07ddc9c58b2d89eea
Marlon <marlonsmoreira@gmail.com>
2020-06-16 14:08:07 -0300

    first commit

---
+++ Entities/Exceptions/DomainException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+
+namespace Course.Entities.Exceptions
+{
+
+    class DomainException : ApplicationException
+    {
+
+
+
+        public DomainException(string message) : base(message)
+        {
+
+        }
+
+    }
+
+}
